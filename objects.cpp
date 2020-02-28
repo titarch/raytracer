06d7a0bf0e01dc4738851cdd883bcf1cd71afa67
Baptiste Parsy <baptiste.parsy@epita.fr>
2020-02-28 11:04:44 +0100

    Add triangles

--- objects.cpp
+++ objects.cpp
@@ -37,9 +37,35 @@ float Plane::intersects(const Line& line) const {
 }
 
 Line Plane::get_normal(const Point& p) const {
-    return {p , norm_};
+    return {p, norm_};
 }
 
 TexPixel Plane::get_tex(const Point& p) const {
+    return tex_.get_tex(p.x(), p.y());
+}
+
+float Triangle::intersects(const Line& line) const {
+    auto h = line.d ^(-e2_);
+    auto a = e0_ * h;
+    if (std::fabs(a) < EPS) return -1;
+    float f = 1.f / a;
+    auto s = line.o - v0_;
+    float u = f * (s * h);
+    if (u < 0.0 || u > 1.0)
+        return -1;
+    auto q = s ^e0_;
+    float v = f * line.d * q;
+    if (v < 0.0 || u + v > 1.0) return -1;
+    float t = f * (-e2_ * q);
+    if (t < EPS) return -1;
+
+    return t;
+}
+
+Line Triangle::get_normal(const Point& p) const {
+    return {p, norm_};
+}
+
+TexPixel Triangle::get_tex(const Point& p) const {
     return tex_.get_tex(p.x(), p.y());
 }
